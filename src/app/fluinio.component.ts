0c1812b83305243565b774379a5f1aff98977368
Stephen Fluin <stephen.fluin@gmail.com>
2016-08-06 11:15:54 -0700

    cleanup and preparation for ga

--- src/app/fluinio.component.ts
+++ src/app/fluinio.component.ts
@@ -1,7 +1,17 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+
+declare var ga : any;
 
 @Component({
   selector: 'app-root',
   templateUrl: 'fluinio.component.html',
 })
-export class FluinioAppComponent {}
+export class FluinioAppComponent {
+  constructor(private router : Router ) {
+    /*router.events.filter(e => e instanceof NavigationEnd).subscribe( (n:NavigationEnd) => { 
+      ga('send', 'pageview', n.urlAfterRedirects);
+    });*/
+  }
+}
