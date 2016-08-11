03c28e73303da7741e2559b8af033f8977f43103
Stephen Fluin <stephen.fluin@gmail.com>
2016-08-10 22:52:07 -0700

    Added content and markdown processing

--- src/app/pages/blog-post.component.ts
+++ src/app/pages/blog-post.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute } from '@angular/router';
 import { Http } from '@angular/http';
 import { Observable } from 'rxjs';
 
+import * as Showdown from 'showdown';
+
 @Component({
   templateUrl: './views/blog-post.component.html',
 })
@@ -12,10 +14,14 @@ export class BlogPostComponent  {
     constructor(activatedRoute : ActivatedRoute, http: Http) {
         activatedRoute.params.flatMap((params) => {
             return http.get('/posts.json').map(response => {
-                return (response.json() as any[]).find(item => 
+                let item = (response.json() as any[]).find(item => 
                     item.id === params['id']
-                ) 
+                );
+                let converter = new Showdown.Converter();
+                item.body = converter.makeHtml(item.body);
+                return item;
             })
         }).subscribe(post => this.post = post);
+
     }
 }
