dbf45afb9a7e741d9feeba4e249511aadde2851b
Stephen Fluin <stephen.fluin@gmail.com>
2016-08-11 21:35:32 -0700

    Blog links and content

--- src/app/pages/blog.component.ts
+++ src/app/pages/blog.component.ts
@@ -1,6 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs';
 
 @Component({
   templateUrl: './views/blog.component.html',
 })
-export class BlogComponent  { }
+export class BlogComponent  {
+  posts: Observable<any[]>;
+  constructor(http: Http) {
+      this.posts = http.get('/posts.json')
+      .map(response => {
+        let result = response.json() as any[];
+        return result.splice(0,4);
+      });
+  }
+ }
