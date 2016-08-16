3433ad12edc8584c3ccbeb3e9fe893a598ee5c9e
Stephen Fluin <stephen.fluin@gmail.com>
2016-08-16 11:35:19 -0700

    Added missing deps for showdown and types

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,6 +29,7 @@ import { routeConfig } from './routes';
         HomeComponent,
         BlogComponent,
         SpeakingComponent,
+        BioComponent,
         BlogHomeComponent,
         BlogPostComponent,
         ProjectsComponent,
