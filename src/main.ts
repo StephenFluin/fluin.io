77cb2f3a8441b631632557eabb2a9122f353accd
Stephen Fluin <stephen.fluin@gmail.com>
2016-12-21 09:38:45 -0800

    CLI cleanup for going to production

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,13 @@
-main.jit.ts
+import './polyfills.ts';
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from '@angular/core';
+import { environment } from './environments/environment';
+import { AppModule } from './app/app.module';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule);
+
