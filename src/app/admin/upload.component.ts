395e80d9fba040ceb828692f69b35bbd631f2030
Stephen Fluin <stephen.fluin@gmail.com>
2016-12-27 14:00:22 -0800

    Fix firebase storage promise resolution

--- src/app/admin/upload.component.ts
+++ src/app/admin/upload.component.ts
@@ -25,7 +25,8 @@ export interface Image {
     <h2>File Gallery</h2>
     <div style="overflow:hidden;">
         <div *ngFor="let img of imageList | async" style="position:relative;width:100px;height:100px;float:left;display:flex;justify-content:center;align-items:center;">
-            <img *ngIf="img.downloadURL | async" [src]="img.downloadURL | async" style="max-width:100px;max-height:100px;">
+            <img *ngIf="img && img.downloadURL && img.downloadURL | async" [src]="img.downloadURL | async" style="max-width:100px;max-height:100px;">
+
             <button (click)="delete(img)" style="position:absolute;top:2px;right:2px;">[x]</button>
         </div>
     </div>
@@ -37,28 +38,30 @@ export class UploadComponent {
      * eg. posts/angular-is-awesome
      */
     @Input() folder: string;
-    
-    fileList : FirebaseListObservable<Image[]>;
-    imageList : Observable<Image[]>;
+
+    fileList: FirebaseListObservable<Image[]>;
+    imageList: Observable<Image[]>;
 
     constructor(public af: AngularFire, public router: Router) {
     }
     ngOnInit() {
-        
+
     }
 
     ngOnChanges() {
         console.log("new values for folder");
         let storage = firebase.storage();
-        
+
         this.fileList = this.af.database.list(`/${this.folder}/images`);
-        console.log("Rendering all images in ",`/${this.folder}/images`)
-        this.imageList = this.fileList.map( itemList =>
-            itemList.map( item => {
+        console.log("Rendering all images in ", `/${this.folder}/images`)
+        this.imageList = this.fileList.map(itemList =>
+            itemList.map(item => {
                 var pathReference = storage.ref(item.path);
-		console.log("path ref is:",pathReference);
-                let result = {$key: item.$key, downloadURL: pathReference.getDownloadURL(), path: item.path, filename: item.filename};
-                console.log(result);
+                let result = { $key: item.$key, path: item.path, downloadURL: null, filename: item.filename };
+                // This Promise must be wrapped in Promise.resolve because the thennable from 
+                // firebase isn't monkeypatched by zones and therefore doesn't trigger CD
+                result.downloadURL = Promise.resolve(pathReference.getDownloadURL());
+                
                 return result;
             })
         );
@@ -71,14 +74,14 @@ export class UploadComponent {
 
         let success = false;
 
-        if((<HTMLInputElement>document.getElementById('file')).files.length <= 0) {
+        if ((<HTMLInputElement>document.getElementById('file')).files.length <= 0) {
             console.log("No files found to upload.");
             return;
         }
 
         // This currently only grabs item 0, TODO refactor it to grab them all
         for (let selectedFile of [(<HTMLInputElement>document.getElementById('file')).files[0]]) {
-            
+
             console.log(selectedFile);
             // Make local copies of services because "this" will be clobbered
             let router = this.router;
@@ -87,11 +90,11 @@ export class UploadComponent {
             let path = `/${this.folder}/${selectedFile.name}`;
             var iRef = storageRef.child(path);
             iRef.put(selectedFile).then((snapshot) => {
-                console.log('Uploaded a blob or file! Now storing the reference at',`/${this.folder}/images/`);
+                console.log('Uploaded a blob or file! Now storing the reference at', `/${this.folder}/images/`);
                 af.database.list(`/${folder}/images/`).push({ path: path, filename: selectedFile.name })
             });
         }
-        
+
     }
     delete(image: Image) {
         let storagePath = image.path;
@@ -101,15 +104,15 @@ export class UploadComponent {
 
         // Delete from Storage
         firebase.storage().ref().child(storagePath).delete()
-        .then(
-            () => {},
-            (error) => console.error("Error deleting stored file",storagePath)
-        );
+            .then(
+            () => { },
+            (error) => console.error("Error deleting stored file", storagePath)
+            );
 
         // Delete references
         this.af.database.object(referencePath).remove()
-            
-        
+
+
 
     }
 }
