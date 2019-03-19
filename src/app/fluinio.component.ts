9666bc0c36dbd6a547297705f369504245593ccd
Stephen Fluin <stephen.fluin@gmail.com>
2019-03-19 08:39:17 -0700

    add fullstory

--- src/app/fluinio.component.ts
+++ src/app/fluinio.component.ts
@@ -38,8 +38,8 @@ declare var ga: any;
                 ]),
                 // animate('1s', style({ height: '*'})),
             ]),
-        ])
-    ]
+        ]),
+    ],
 })
 export class FluinioAppComponent {
     /**
@@ -52,7 +52,6 @@ export class FluinioAppComponent {
             this.updateHeight();
         }
         return this._maxHeight;
-
     }
     @ViewChild('container') container: ElementRef;
 
@@ -74,6 +73,57 @@ export class FluinioAppComponent {
                 this._maxHeight = null;
             }
         });
+
+        // Setup Fullstory
+        window['_fs_debug'] = false;
+        window['_fs_host'] = 'fullstory.com';
+        window['_fs_org'] = 'K11CD';
+        window['_fs_namespace'] = 'FS';
+        (function(m, n, e, t, l, o, g, y) {
+            if (e in m) {
+                if (m.console && m.console.log) {
+                    m.console.log('FullStory namespace conflict. Please set window["_fs_namespace"].');
+                }
+                return;
+            }
+            g = m[e] = function(a, b, s) {
+                g.q ? g.q.push([a, b, s]) : g._api(a, b, s);
+            };
+            g.q = [];
+            o = n.createElement(t);
+            o.async = 1;
+            o.src = 'https://' + window['_fs_host'] + '/s/fs.js';
+            y = n.getElementsByTagName(t)[0];
+            y.parentNode.insertBefore(o, y);
+            g.identify = function(i, v, s) {
+                g(l, { uid: i }, s);
+                if (v) {
+                    g(l, v, s);
+                }
+            };
+            g.setUserVars = function(v, s) {
+                g(l, v, s);
+            };
+            g.event = function(i, v, s) {
+                g('event', { n: i, p: v }, s);
+            };
+            g.shutdown = function() {
+                g('rec', !1);
+            };
+            g.restart = function() {
+                g('rec', !0);
+            };
+            g.consent = function(a) {
+                g('consent', !arguments.length || a);
+            };
+            g.identifyAccount = function(i, v) {
+                o = 'account';
+                v = v || {};
+                v.acctId = i;
+                g(o, v);
+            };
+            g.clearUserCookie = function() {};
+        })(window, document, window['_fs_namespace'], 'script', 'user');
     }
 
     /** Get the current page for route animation purposes */
@@ -93,5 +143,4 @@ export class FluinioAppComponent {
 
         this._maxHeight = maxHeight + 'px';
     }
-
 }
