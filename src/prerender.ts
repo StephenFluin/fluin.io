b9e4954940704518e8f5dd4cd3c0a0518be8e1e8
Stephen Fluin <stephen.fluin@gmail.com>
2019-08-21 15:40:33 -0700

    WIP prerendering 2

--- src/prerender.ts
+++ src/prerender.ts
@@ -1,23 +1,44 @@
 import * as Prerenderer from 'puppeteer-prerender';
 import * as fs from 'fs';
+import * as child_process from 'child_process';
+
+async function main() {
+    console.log('PRERENDER Running ng build --prod');
+    // const build = child_process.spawn('ng', ['build', '--prod']);
+    const build = child_process.spawn('ls');
+    // build.stdout.on('data', data => console.log('PRERENDER ng build output:', data));
+    build.on('exit', () => {
+        console.log('PRERENDER prod build complete');
+        console.log('PRERENDER setting up server');
+        const cp = child_process.spawn('npx', ['http-server', 'dist', '-p8000', '--proxy','http://localhost:8000?']);
+        cp.stdout.on('data', async data => {
+            console.log(`stdout data: ${data}`);
+            if (/Available on/.test(data)) {
+                setTimeout(async () => {
+                    console.log('PRERENDER server ready');
+                // Start rendering after server is up
+                await render('http://localhost:8000/blog/5-things-to-do-after-ng-new', 'dist');
+                //await render('http://woot.com', 'dist');
+
+                // cp.kill();
+                },1000);
+            }
+        });
+        cp.stderr.on('data', data => console.error(`stderr data: ${data}`));
+    });
+}
 
 async function render(url, destination) {
-    const prerender = new Prerenderer();
+    const path = /^.*\/\/.*?\/(.*)$/.exec(url)[1];
+    console.log('PRERENDER prerendering',url,'and', path);
+
+    const prerender = new Prerenderer({debug:true,timeout:30000,followRedirect:true});
 
     try {
-        const {
-            status,
-            redirect,
-            meta,
-            openGraph,
-            links,
-            html,
-            staticHTML
-        } = await prerender.render(url);
-        console.log('url', url);
-        const path = /^.*\/\/.*?\/(.*)$/.exec(url)[1];
-        console.log('figured out path is',path);
-        fs.writeFileSync(`${destination}/${path}`,staticHTML)
+        const { status, redirect, meta, openGraph, links, html, staticHTML } = await prerender.render('http://localhost:8000/blog/5-things-to-do-after-ng-new');
+        console.log('status was',status,'redirect was',redirect,'meta was',meta);
+        fs.mkdir(`${destination}/blog`, () => {});
+        // fs.writeFileSync(`${destination}/${path}`, staticHTML);
     } catch (e) {
         console.error(e);
     }
@@ -25,4 +46,4 @@ async function render(url, destination) {
     await prerender.close();
 }
 
-render('http://localhost:4200/blog/5-things-to-do-after-ng-new', 'dist');
+main();
--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd functions && ./node_modules/.bin/tsc && cd ..
+
 ng build --prod --named-chunks && yarn run gfp && firebase deploy
