5563998105e84f8fb2cca3e4899666591815e1ea
Stephen Fluin <stephen.fluin@gmail.com>
2019-08-21 12:22:02 -0700

    add metadata tags for twitter

--- src/app/pages/blog-post.component.ts
+++ src/app/pages/blog-post.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Title } from '@angular/platform-browser';
+import { Title, MetaDefinition } from '@angular/platform-browser';
 
 import { DomSanitizer } from '@angular/platform-browser';
 
 import { AdminService } from '../shared/admin.service';
 import { Post, PostService } from '../shared/post.service';
 import { Observable } from 'rxjs';
-import { map, switchMap } from 'rxjs/operators';
+import { map, tap, switchMap } from 'rxjs/operators';
 
 import snarkdown from 'snarkdown';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
     templateUrl: './blog-post.component.html',
@@ -21,8 +22,9 @@ export class BlogPostComponent {
         route: ActivatedRoute,
         posts: PostService,
         title: Title,
+        meta: Meta,
         public adminService: AdminService,
-        private sanitized: DomSanitizer
+        private sanitized: DomSanitizer,
     ) {
         // Based on the requested ID, return a Post
         this.post = route.params.pipe(
@@ -35,10 +37,27 @@ export class BlogPostComponent {
                     return posts.postMap.pipe(map(postMap => postMap[params['id']]));
                 }
             }),
-            map(item => {
+            tap(item => {
                 if (item) {
                     title.setTitle(item.title + ' | fluin.io blog');
+                    const description = item.body.split('\n')[0];
 
+                    const twitterMetadata = {
+                        'twitter:card': 'summary',
+                        'twitter:site': '@stephenfluin',
+                        'twitter:creator': '@stephenfluin',
+                    };
+                    const openGraphMeta = {
+                        'og:url': `https://fluin.io/blog/${item.id}`,
+                        'og:title': `${item.title}`,
+                        'og:description': description,
+                        'og:image': `${item.image}`,
+                    };
+
+                    const tags: MetaDefinition[] = Object.keys(twitterMetadata).map(key => ({ name: key, content: twitterMetadata[key] }));
+                    const tags2: MetaDefinition[] = Object.keys(openGraphMeta).map(key => ({ property: key, content: openGraphMeta[key] }));
+
+                    meta.addTags([...tags, ...tags2]);
 
                     item.renderedBody = sanitized.bypassSecurityTrustHtml(snarkdown(item.body || ''));
                 }
