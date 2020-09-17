b025c20981f3cfb0ba23156a3e01677d21328588
Stephen Fluin <stephen.fluin@gmail.com>
2020-09-17 13:26:15 -0700

    clean up rules and imports

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { AuthService } from './shared/auth.service';
 import { AngularFireDatabase } from '@angular/fire/database';
 import { map } from 'rxjs/operators';
