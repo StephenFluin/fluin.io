a58f0d7e27cf85674886ee01a018a18046e09725
Stephen Fluin <stephen.fluin@gmail.com>
2021-02-03 14:51:33 -0800

    fix linting

--- src/app/newsletter/newsletter/newsletter.component.ts
+++ src/app/newsletter/newsletter/newsletter.component.ts
@@ -1,21 +1,17 @@
-import { Component, ElementRef, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-newsletter',
-  templateUrl: './newsletter.html',
+    selector: 'app-newsletter',
+    templateUrl: './newsletter.html',
 })
-export class NewsletterComponent implements OnInit {
+export class NewsletterComponent implements OnInit, AfterViewInit {
+    constructor(private elRef: ElementRef) {}
 
-  constructor(private elRef:ElementRef) { }
-
-  ngOnInit(): void {
-  }
-  ngAfterViewInit() {
-  //<script src="https://f.convertkit.com/ckjs/ck.5.js"></script>
-    const s= document.createElement('script');
-    s.src = 'https://f.convertkit.com/ckjs/ck.5.js';
-    this.elRef.nativeElement.appendChild(s);
-
-  }
-
+    ngOnInit(): void {}
+    ngAfterViewInit() {
+        // <script src="https://f.convertkit.com/ckjs/ck.5.js"></script>
+        const s = document.createElement('script');
+        s.src = 'https://f.convertkit.com/ckjs/ck.5.js';
+        this.elRef.nativeElement.appendChild(s);
+    }
 }
