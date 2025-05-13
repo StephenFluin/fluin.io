c884390184b5fdf69b34081d71b5427342568e0d
Stephen Fluin <stephen.fluin@gmail.com>
2025-05-13 06:45:34 -0500

    fix: add comment to find firebase problem.

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
 import {
-  AngularNodeAppEngine,
-  createNodeRequestHandler,
-  isMainModule,
-  writeResponseToNodeResponse,
+    AngularNodeAppEngine,
+    createNodeRequestHandler,
+    isMainModule,
+    writeResponseToNodeResponse,
 } from '@angular/ssr/node';
 import express from 'express';
 import { join } from 'node:path';
@@ -28,23 +28,21 @@ const angularApp = new AngularNodeAppEngine();
  * Serve static files from /browser
  */
 app.use(
-  express.static(browserDistFolder, {
-    maxAge: '1y',
-    index: false,
-    redirect: false,
-  }),
+    express.static(browserDistFolder, {
+        maxAge: '1y',
+        index: false,
+        redirect: false,
+    })
 );
 
 /**
  * Handle all other requests by rendering the Angular application.
  */
 app.use((req, res, next) => {
-  angularApp
-    .handle(req)
-    .then((response) =>
-      response ? writeResponseToNodeResponse(response, res) : next(),
-    )
-    .catch(next);
+    angularApp
+        .handle(req)
+        .then((response) => (response ? writeResponseToNodeResponse(response, res) : next()))
+        .catch(next);
 });
 
 /**
@@ -52,17 +50,19 @@ app.use((req, res, next) => {
  * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
  */
 if (isMainModule(import.meta.url)) {
-  const port = process.env['PORT'] || 4000;
-  app.listen(port, (error) => {
-    if (error) {
-      throw error;
-    }
+    const port = process.env['PORT'] || 4000;
+    app.listen(port, (error) => {
+        if (error) {
+            throw error;
+        }
 
-    console.log(`Node Express server listening on http://localhost:${port}`);
-  });
+        console.log(`Node Express server listening on http://localhost:${port}`);
+    });
 }
 
 /**
  * Request handler used by the Angular CLI (for dev-server and during build) or Firebase Cloud Functions.
  */
 export const reqHandler = createNodeRequestHandler(app);
+
+console.log('App request handler created');
