8e939d56a07b6a362953211d938e3e5b3fb4178a
Stephen Fluin <stephen.fluin@gmail.com>
2025-10-19 00:05:23 -0400

    chore: angular version bump

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,9 +1,9 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication, BootstrapContext } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
 import { provideNoopAnimations } from '@angular/platform-browser/animations';
 
-const bootstrap = () =>
-    bootstrapApplication(AppComponent, { providers: [...config.providers, provideNoopAnimations()] });
+const bootstrap = (context: BootstrapContext) =>
+    bootstrapApplication(AppComponent, { providers: [...config.providers, provideNoopAnimations()] }, context);
 
 export default bootstrap;
