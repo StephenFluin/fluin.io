81f35a841a8573be38da5664439f3d04e8a7f055
Stephen Fluin <stephen.fluin@gmail.com>
2025-11-20 11:18:18 -0600

    chore: bump to angular v21

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,9 +1,10 @@
+import { provideZoneChangeDetection } from "@angular/core";
 import { bootstrapApplication, BootstrapContext } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
 import { provideNoopAnimations } from '@angular/platform-browser/animations';
 
 const bootstrap = (context: BootstrapContext) =>
-    bootstrapApplication(AppComponent, { providers: [...config.providers, provideNoopAnimations()] }, context);
+    bootstrapApplication(AppComponent, { providers: [provideZoneChangeDetection(),...config.providers, provideNoopAnimations()] }, context);
 
 export default bootstrap;
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
+import { provideZoneChangeDetection } from "@angular/core";
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
-bootstrapApplication(AppComponent, { providers: [...appConfig.providers, provideAnimationsAsync()] }).catch((err) =>
+bootstrapApplication(AppComponent, { providers: [provideZoneChangeDetection(),...appConfig.providers, provideAnimationsAsync()] }).catch((err) =>
     console.error(err)
 );
