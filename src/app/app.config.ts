7772828f8242b34a9bc2d0ea1ea9cb07197cea03
Stephen Fluin <stephen.fluin@gmail.com>
2026-04-23 02:48:14 -0700

    feat: lazy loading, dead code, and other perf improvements

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,12 @@
 import {
     ApplicationConfig,
     provideBrowserGlobalErrorListeners,
-    provideCheckNoChangesConfig,
     provideZonelessChangeDetection,
 } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration, Title, withEventReplay, withNoHttpTransferCache } from '@angular/platform-browser';
+import { provideClientHydration, Title, withEventReplay } from '@angular/platform-browser';
 import { AdminService } from './shared/admin.service';
 import { PostService } from './shared/post.service';
 import { provideHttpClient, withFetch } from '@angular/common/http';
@@ -19,9 +18,8 @@ export const appConfig: ApplicationConfig = {
         AdminService,
         provideBrowserGlobalErrorListeners(),
         provideZonelessChangeDetection(),
-        provideCheckNoChangesConfig({ exhaustive: true, interval: 1000 }),
-        provideRouter(routes),
-        provideClientHydration(withEventReplay(), withNoHttpTransferCache()),
+        provideRouter(routes, withPreloading(PreloadAllModules)),
+        provideClientHydration(withEventReplay()),
         provideHttpClient(withFetch()),
     ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,32 +1,35 @@
 import { Routes } from '@angular/router';
 
-import { NoBlogComponent } from './no-blog.component';
-import { NotFoundComponent } from './not-found.component';
-import { BioComponent } from './pages/bio.component';
-import { BlogPostComponent } from './pages/blog-post.component';
-import { BlogComponent } from './pages/blog.component';
-import { HomeComponent } from './pages/home.component';
-import { ProjectsComponent } from './pages/projects.component';
-import { Send404Component } from './send-404.component';
-
 export const routes: Routes = [
-    { path: '', component: HomeComponent, data: { title: 'fluin.io - Stephen Fluin. CPO, Product, DevRel, Speaker, and more', page: 'home' } },
+    {
+        path: '',
+        loadComponent: () => import('./pages/home.component').then((m) => m.HomeComponent),
+        data: { title: 'fluin.io - Stephen Fluin. CPO, Product, DevRel, Speaker, and more', page: 'home' },
+    },
     {
         path: 'blog',
         data: { title: false, page: 'blog' },
-        component: BlogComponent,
+        loadComponent: () => import('./pages/blog.component').then((m) => m.BlogComponent),
         children: [
-            { path: '', component: NoBlogComponent },
-            { path: ':id', component: BlogPostComponent },
+            { path: '', loadComponent: () => import('./no-blog.component').then((m) => m.NoBlogComponent) },
+            { path: ':id', loadComponent: () => import('./pages/blog-post.component').then((m) => m.BlogPostComponent) },
         ],
     },
-    { path: 'bio', component: BioComponent, data: { title: 'About Stephen Fluin' } },
-    { path: 'projects', component: ProjectsComponent, data: { title: 'Projects' } },
+    {
+        path: 'bio',
+        loadComponent: () => import('./pages/bio.component').then((m) => m.BioComponent),
+        data: { title: 'About Stephen Fluin' },
+    },
+    {
+        path: 'projects',
+        loadComponent: () => import('./pages/projects.component').then((m) => m.ProjectsComponent),
+        data: { title: 'Projects' },
+    },
     { path: 'admin', loadChildren: () => import('./admin/admin.routes').then((m) => m.AdminRoutes) },
     {
         path: 'newsletter',
         loadChildren: () => import('./newsletter/newsletter.routes').then((m) => m.routes),
     },
-    { path: '404', component: NotFoundComponent },
-    { path: '**', component: Send404Component },
+    { path: '404', loadComponent: () => import('./not-found.component').then((m) => m.NotFoundComponent) },
+    { path: '**', loadComponent: () => import('./send-404.component').then((m) => m.Send404Component) },
 ];
