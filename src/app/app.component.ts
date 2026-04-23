2cd11caf9aaf37a92b4b55654ca513a53a618858
Stephen Fluin <stephen.fluin@gmail.com>
2026-04-23 04:45:39 -0700

    fix: gtag critical error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { Title, Meta } from '@angular/platform-browser';
 import { Router, NavigationEnd, RouterOutlet, RouterLink } from '@angular/router';
 import { filter } from 'rxjs/operators';
 import { AppHeaderComponent } from './embeddable/app-header.component';
 
-declare let ga: any;
-declare let gtag: any;
+declare global {
+    interface Window {
+        gtag: (...args: any[]) => void;
+    }
+}
 
 @Component({
     selector: 'app-root',
@@ -14,6 +18,11 @@ declare let gtag: any;
 })
 export class AppComponent {
     constructor(router: Router, title: Title, meta: Meta) {
+        const isBrowser = isPlatformBrowser(inject(PLATFORM_ID));
+        const gtag = isBrowser ? window.gtag : () => {};
+
+        gtag('config', 'G-QMN47NKMMS');
+
         router.events.pipe(filter((e) => e instanceof NavigationEnd)).subscribe((n: NavigationEnd) => {
             const pageTitle = router.routerState.snapshot.root.children[0].data['title'];
             if (pageTitle) {
@@ -22,13 +31,10 @@ export class AppComponent {
                 title.setTitle('fluin.io');
             }
             meta.removeTag('name=robots');
-            try {
+            if (isBrowser) {
                 window.scrollTo(0, 0);
-                ga('send', 'pageview', n.urlAfterRedirects);
-                gtag('config', 'G-QMN47NKMMS', { page_path: n.urlAfterRedirects });
-            } catch (err) {
-                // Maybe not in a browser?
             }
+            gtag('config', 'G-QMN47NKMMS', { page_path: n.urlAfterRedirects });
         });
     }
 }
